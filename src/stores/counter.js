16a579c4e3c8848ff770b3e9551055d8bebfa313
saknarin818 <saknarin828@gmail.com>
2023-10-06 18:00:55 +0700

    first commit

--- src/stores/counter.js
+++ src/stores/counter.js
@@ -4,11 +4,11 @@ import { defineStore } from 'pinia'
 export const menuStore = defineStore('menu', () => {
 
   const menu = ref([
-    { name: 'สินค้า1 ', img: '', price: 120 },
-    { name: ' สินค้า2 ', img: '', price: 80 },
-    { name: ' สินค้า3 ', img: '', price: 60 },
-    { name: 'สินค้า4 ', img: '', price: 40 },
-    { name: ' สินค้า5 ', img: '', price: 90 },
+    { name: 'เมนูผัดกระเพรา ', img: 'https://s359.kapook.com//pagebuilder/2cc482f6-9e80-488f-9e0a-87103f16b40d.jpg', price: 60 },
+    { name: ' เมนูผัดพริกแกง ', img: 'https://s359.kapook.com//pagebuilder/9dbc7505-3b39-4b7f-85b4-1c88c2a01e7f.jpg', price: 50 },
+    { name: ' เมนูผัดผัก    ', img: 'https://s359.kapook.com//pagebuilder/6ef91549-ce57-47d6-88db-3ca0c16d1b9e.jpg', price: 50 },
+    { name: 'เมนูผัดผงกะหรี่ ', img: 'https://s359.kapook.com//pagebuilder/efa12ebe-add1-4348-bbae-0adf4f4766c7.jpg', price: 60 },
+    { name: ' เมนูผัดกระเทียม / ทอดกระเทียม ', img: 'https://s359.kapook.com//pagebuilder/7d2111b0-1a61-4196-a0fe-f77eb122612f.jpg', price: 50 },
 
 
   ]);
